4e082d9429add8cfd6f05c14304bcf079428a8d9
Rouabah <rouabah.zineb@hotmail.fr>
2023-10-09 11:23:36 +0200

    ajout de la fléche retour en arrière

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -16,3 +16,63 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+	const nombreSlides = slides.length
+ 	console.log(nombreSlides);
+
+ 	let arrow_left = document.querySelector (".arrow_left");
+arrow_left.addEventListener("click", () => {
+		console.log("page précedente");
+	})
+
+let arrow_right = document.querySelector (".arrow_right");	
+	arrow_right.addEventListener("click", () => {
+   		console.log("page suivante");
+})
+ 
+
+let diaporama=1
+function change(n) {
+	(diaporama = n);
+  }
+let dots = document.getElementsByClassName("dot");
+
+	for(i=0 ;i<dots.length;i++){
+	console.log(dots[0])
+// dots[i].className = dots[i].className.replace(" dot_selected", "");
+// dots[diaporama-1].className += " dot_selected";
+}
+
+
+	
+
+	
+	
+
+
+	
+
+/*let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides (slideIndex +=n)
+}
+function currentSlide(n) {
+	showSlides (slideIndex = n)
+}
+
+function showSlides(n){
+	let i;
+	let slides = document.getElementsByClassName(".banner-img");
+	let dots = document.querySelectorAll(".dots");
+
+if(n > slides.length) {slideIndex=1}
+if (n < 1) {slideIndex= slides.length}
+}
+
+for(let i= 0; i < dots.length; i++) {
+	dots[i].classList.remove("active");
+}
+slides[slideIndex -1].style.display ="block";
+
+dots[slideIndex -1].classList.add("active");*/
