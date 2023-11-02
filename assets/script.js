4c3cff8bbc30fc06782a54c672a37bbe649d9888
Rouabah <rouabah.zineb@hotmail.fr>
2023-11-02 12:02:53 +0100

    modification dot

--- assets/script.js
+++ assets/script.js
@@ -33,7 +33,7 @@ slides.forEach((slide, index) => {
     dot.addEventListener("click", () => showSlide(index));
     dotsContainer.appendChild(dot);
     dots.push(dot); 
-	if (index===0){
+		if (index===0){
 		dot.classList.add("dot_selected");
 	}
 });
